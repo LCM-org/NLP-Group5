1d63a5a2b0bbc23f27c182a9e76de91ac523c776
abbas5253 <abbas.ism53@gmail.com>
2023-11-10 18:46:56 -0500

    add action script code

---
+++ main.py
@@ -0,0 +1,22 @@
+from cleaning.clean_dataset import *
+from feature_selection.get_features import *
+from models.model import *
+from preprocessing.process_dataset import *
+
+def main():
+    """
+    1. Export CSV dataset to pandas dataframe
+    2. data cleaning
+    3. data preprocessing
+    4. feature engineering
+    5. Model building
+    6. Model validation
+    7. Model Inference through Flask
+    """
+    raise NotImplementedError(
+        "Hate speech detection project in progress..."
+    )
+
+
+if __name__ == '__main__':
+    main()
