b4b99ab3be56e3666f953cb654008a47103d74ec
NavneetV98 <125153948+NavneetV98@users.noreply.github.com>
2023-11-14 12:45:55 -0500

    Update Bag of words model.py

--- feature_selection/Bag of words model.py
+++ feature_selection/Bag of words model.py
@@ -6,13 +6,13 @@ df = pd.read_csv(r"C:\Users\Owner\OneDrive\Documents\measuring_hate_speech.csv")
 
 def create_bow_matrix(texts):
     # Create a CountVectorizer instance
-    vectorizer = CountVectorizer(max_features=5000)
+    vectorizer = CountVectorizer(max_features=6000)
 
     # Fit and transform the texts to get the BoW representation
-    bow_matrix = vectorizer.fit_transform(texts)
+    bow_Matrix = vectorizer.fit_transform(texts)
 
     # Convert the BoW matrix to a DataFrame for better visualization
-    bow_df = pd.DataFrame(bow_matrix.toarray(), columns=vectorizer.get_feature_names_out())
+    bow_df = pd.DataFrame(bow_Matrix.toarray(), columns=vectorizer.get_feature_names_out())
 
     # Return the BoW DataFrame
     return bow_df
