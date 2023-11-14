02a1d8b3b0f9565ccbdfec1cdb1cd457b8631f08
NavneetV98 <125153948+NavneetV98@users.noreply.github.com>
2023-11-14 14:02:00 -0500

    Update expanding_contractions.py

--- cleaning/expanding_contractions.py
+++ cleaning/expanding_contractions.py
@@ -6,20 +6,20 @@
 import pandas as pd
 import contractions
 
-#Reading csv file 
+
+#reading csv file.......
 df = pd.read_csv("measuring_hate_speech.csv")
-print(df.head())
 
-
-def expand_contractions(text):
-   expanded_text = contractions.fix(text)
-   return expanded_text
-
-# Apply expand contraction function to text column 
-df['expanded_text'] = df['text'].apply(expand_contractions)
+#defining contraction function...
+def expand_contractions(df):
+  df= df['text'].apply(contractions.fix)
+  return df
 
 # Display the original and expanded text side by side
-print(df[['text', 'expanded_text']])
+print(df['text'])
+
+expand_df = expand_contractions(df)
+print(expand_df['text'])
 
 
 
