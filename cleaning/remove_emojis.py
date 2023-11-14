627639956f8bb55b03fa5d6245a7571a06f412ef
Sharon Victor <150480858+SharonVictor123@users.noreply.github.com>
2023-11-14 13:38:35 -0500

    Updated remove_emojis.py

--- cleaning/remove_emojis.py
+++ cleaning/remove_emojis.py
@@ -2,14 +2,17 @@
 import pandas as pd
 import emoji
 
-def remove_emojis(text):
-    return emoji.demojize(text)
+def remove_emojis(df):
+    # Apply the emoji.demojize function to the 'text' column
+    df['text'] = df['text'].apply(lambda x: emoji.demojize(x))
+    return df
 
 # Load CSV data into a DataFrame
 df = pd.read_csv('measuring_hate_speech.csv')
 
-# Apply the remove_emojis function to the relevant column(s)
-df['text'] = df['text'].apply(remove_emojis)
+# Call the remove_emojis function to modify the DataFrame
+cleaned_df = remove_emojis(df)
 
-print(df['text'])
+# Display the modified 'text' column
+print(cleaned_df['text'])
 
