0614127051a8208ee6b154cf6a06d15d4de45a28
Simran199628 <125153906+Simran199628@users.noreply.github.com>
2023-11-14 00:17:17 -0500

    Add files via upload

---
+++ heatmap.py
@@ -0,0 +1,29 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd 
+
+
+#Reading csv file 
+df = pd.read_csv(r"C:\Users\Owner\OneDrive\Documents\measuring_hate_speech.csv")
+
+
+numerical_df = df.select_dtypes(include='number')
+
+def plot_heatmap(df):
+    # Compute the correlation matrix
+    correlation_matrix = df.corr()
+
+    # Create a heatmap
+    plt.figure(figsize=(12, 10))
+    sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', fmt='.2f', linewidths=.5)
+
+    # Display the plot
+    plt.title('Correlation Heatmap for Numerical Columns')
+    plt.show()
+
+    # Return the correlation matrix
+    return correlation_matrix
+
+# Plot the heatmap for numerical columns and get the correlation matrix
+correlation_matrix = plot_heatmap(numerical_df)
+
