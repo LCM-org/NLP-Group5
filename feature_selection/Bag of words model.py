83f0f9e70ca539737e358cb1c8c445d455ba7625
NavneetV98 <125153948+NavneetV98@users.noreply.github.com>
2023-11-14 21:09:19 -0500

    Update Bag of words model.py

--- feature_selection/Bag of words model.py
+++ feature_selection/Bag of words model.py
@@ -1,27 +1,19 @@
 from sklearn.feature_extraction.text import CountVectorizer
 import pandas as pd
 
-#Reading csv file 
-df = pd.read_csv("measuring_hate_speech.csv")
-
-def create_bow_matrix(texts):
+def create_bow_matrix(df):
     # Create a CountVectorizer instance
     vectorizer = CountVectorizer(max_features=5000)
-
-    # Fit and transform the texts to get the BoW representation
+    texts = df['text'].tolist()
     bow_matrix = vectorizer.fit_transform(texts)
-
-    # Convert the BoW matrix to a DataFrame for better visualization
     bow_df = pd.DataFrame(bow_matrix.toarray(), columns=vectorizer.get_feature_names_out())
 
     # Return the BoW DataFrame
     return bow_df
 
-# Assuming your dataset has a column named 'text'
-texts = df['text'].tolist()
+# Read your DataFrame from the CSV file
+df = pd.read_csv("measuring_hate_speech.csv")
 
-# Call the function to create the BoW matrix
-bow_matrix_df = create_bow_matrix(texts)
-
-# Display the BoW DataFrame
+# Call the function and display the BoW DataFrame
+bow_matrix_df = create_bow_matrix(df)
 print(bow_matrix_df)
